f5087b016d41fbe653bd6ed65f0f4f81e7ad9d18
Wepres <mohss7378@gmail.com>
2025-04-14 04:06:55 +0200

    changed router to make it visible to github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import styles from './App.module.css';
+// import ScrollToTop from './components/Home/ScrollToTop';
+// import Home from './pages/Home/Home';
+
+// function App() {
+//   return (
+//     <Router>
+//       <ScrollToTop />
+//       <div className={styles.wrapper}>
+//         <div className={styles.content}>
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//           </Routes>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styles from './App.module.css';
 import ScrollToTop from './components/Home/ScrollToTop';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/SCAS-Construction-And-Finishing-ReactJS/">
       <ScrollToTop />
       <div className={styles.wrapper}>
         <div className={styles.content}>
@@ -14,7 +35,7 @@ function App() {
           </Routes>
         </div>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
