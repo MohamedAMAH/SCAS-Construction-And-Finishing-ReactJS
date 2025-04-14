62567c244fb6827c52a8112d73e48d9b7b5f266f
Wepres <mohss7378@gmail.com>
2025-04-14 03:09:02 +0200

    Initital demo with Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/SCAS-Construction-And-Finishing-ReactJS/', // Replace with your repo name
 })
